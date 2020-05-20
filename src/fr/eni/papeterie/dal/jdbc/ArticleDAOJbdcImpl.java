12aac526b6a5f0206017cddbe4e44493160149d7
Vanesa Delage <vanessa.delage@gmail.com>
2020-05-20 15:41:00 +0200

    Ajout fichiers : - AppliTestDAL - ArticleDAOJdbcImpl - .jar

---
+++ src/fr/eni/papeterie/dal/jdbc/ArticleDAOJbdcImpl.java
@@ -0,0 +1,22 @@
+package fr.eni.papeterie.dal.jdbc;
+
+import fr.eni.papeterie.bo.Article;
+
+import java.util.List;
+
+public class ArticleDAOJdbcImpl {
+
+
+    public void insert(Article a1) {
+    }
+
+
+    public Article selectById(Integer idArticle) {
+    }
+
+    public List<Article> selectAll() {
+    }
+
+    public void update(Article a1) {
+    }
+}
